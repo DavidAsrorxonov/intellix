c8585954808227862ee392200e6572c8a3b8f468
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-04-19 22:11:56 +0900

    add Crosshair component and integrate it into the RootDocument layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,25 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import { LogIn } from "lucide-react";
+import { Link } from "@tanstack/react-router";
+
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="brand">
+      <div className="mark">
+        <div className="glyph" />
+      </div>
+
+      <Link to="/">
+        <span>Intellix</span>
+      </Link>
+    </div>
+
+    <div className="actions">
+      <Link to="/sign-in/$" className="btn-primary">
+        <LogIn size="16" />
+        Sign in
+      </Link>
+    </div>
+  </nav>
+);
 
 export default Navbar;
